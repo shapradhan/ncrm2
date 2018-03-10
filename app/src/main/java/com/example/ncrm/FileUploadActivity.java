781548d6c5c5af2a6953f2483c8d2f12ade12fc3
shapradhan <shprnprog@gmail.com>
2018-03-10 21:14:05 +0100

    Add layout for file upload, add functionality to implement file upload to Firebase

--- app/src/main/java/com/example/ncrm/FileUploadActivity.java
+++ app/src/main/java/com/example/ncrm/FileUploadActivity.java
@@ -1,19 +1,120 @@
 package com.example.ncrm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 /**
  * Created by shameer on 2018-03-09.
  */
 
 public class FileUploadActivity extends MainActivity {
+    private static final int PICK_FILE_REQUEST = 80;
+    private Uri mFilePath;
+    private StorageReference mStorageReference;
+    private ProgressBar mFileUploadProgressBar;
+    private String mFileName;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         FrameLayout frameLayout = (FrameLayout) findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.activity_file_upload, frameLayout);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+        mFileUploadProgressBar =(ProgressBar) findViewById(R.id.fileUploadProgressBar);
+
+        Button selectFileBtn = (Button) findViewById(R.id.selectFileBtn);
+        final Button uploadFileBtn = (Button) findViewById(R.id.uploadFileBtn);
+
+        final EditText fileNameEditText = (EditText) findViewById(R.id.fileNameEditText);
+        final EditText fileDescriptionEditText = (EditText) findViewById(R.id.fileDescriptionEditText);
+
+        selectFileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayFileSelector();
+            }
+        });
+
+        uploadFileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFileName = Utility.getStringFromEditText(fileNameEditText);
+                String fileDescription = Utility.getStringFromEditText(fileDescriptionEditText);
+                uploadFile();
+            }
+        });
+    }
+
+    private void displayFileSelector() {
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), PICK_FILE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Image is selected
+            mFilePath = data.getData();
+        }
+    }
+
+    private void uploadFile() {
+        if (mFilePath != null) {
+            StorageReference fileReference = mStorageReference.child("files/" + mFileName);
+
+            fileReference.putFile(mFilePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                            System.out.println("DU "  + downloadUrl.toString());
+                            mFileUploadProgressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(getApplicationContext(), "File successfully uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            mFileUploadProgressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(getApplicationContext(), "File could not be uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double TOTAL_PERCENTAGE = 100.0;
+                            double sizeTransferredPortion = taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount();
+                            int progressPercentage = (int) (TOTAL_PERCENTAGE * sizeTransferredPortion);
+                            mFileUploadProgressBar.setVisibility(View.VISIBLE);
+                            mFileUploadProgressBar.setProgress(progressPercentage);
+                        }
+                    });
+        }
+        else {
+        }
     }
 }
