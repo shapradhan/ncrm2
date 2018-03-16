eb8b6e1dd4226160c20c9965093bfffc709b3911
shapradhan <shprnprog@gmail.com>
2018-03-16 11:04:19 +0100

    Implement click listeners on edit button and delete button in ReminderAdapter

--- app/src/main/java/com/example/ncrm/ReminderAdapter.java
+++ app/src/main/java/com/example/ncrm/ReminderAdapter.java
@@ -1,11 +1,11 @@
 package com.example.ncrm;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,17 +24,19 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.Remind
     }
 
     @Override
-    public ReminderAdapter.ReminderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ReminderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder_item_view, parent, false);
-        return new ReminderAdapter.ReminderViewHolder(view, mContext, mReminderList);
+        return new ReminderViewHolder(view, mContext, mReminderList);
     }
 
     @Override
     public void onBindViewHolder(ReminderAdapter.ReminderViewHolder holder, int position) {
-        Reminder reminder = mReminderList.get(position);
+        final Reminder reminder = mReminderList.get(position);
         holder.mReminderItem.setText(reminder.getReminderItem());
         holder.mReminderDate.setText(reminder.getReminderDate());
         holder.mReminderTime.setText(reminder.getReminderTime());
+        attachChildListener(holder.mEditBtn, reminder);
+        attachChildListener(holder.mDeleteBtn, reminder);
     }
 
     @Override
@@ -42,10 +44,34 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.Remind
         return mReminderList.size();
     }
 
+    private void attachChildListener(final ImageButton button, final Reminder reminder) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (button.getId()) {
+                    case R.id.editBtn:
+                        editReminder(reminder);
+                        break;
+                    case R.id.deleteBtn:
+                        deleteReminder(reminder);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void editReminder(Reminder reminder) {
+    }
+
+    private void deleteReminder(Reminder reminder) {
+    }
+
     public class ReminderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView mReminderItem;
         TextView mReminderDate;
         TextView mReminderTime;
+        ImageButton mEditBtn;
+        ImageButton mDeleteBtn;
         ArrayList<Reminder> mReminders = new ArrayList<>();
         Context mContext;
 
@@ -57,15 +83,12 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.Remind
             mReminderItem = (TextView) itemView.findViewById(R.id.reminderItem);
             mReminderDate = (TextView) itemView.findViewById(R.id.reminderDate);
             mReminderTime = (TextView) itemView.findViewById(R.id.reminderTime);
+            mEditBtn = (ImageButton) itemView.findViewById(R.id.editBtn);
+            mDeleteBtn = (ImageButton) itemView.findViewById(R.id.deleteBtn);
         }
 
         @Override
         public void onClick(View v) {
-            int position = getAdapterPosition();
-            Reminder reminder = this.mReminders.get(position);
-//            Intent intent = new Intent(mContext, MeetingDetailActivity.class);
-//            intent.putExtra("object", meeting);
-//            mContext.startActivity(intent);
         }
     }
 }
